5d17cb1fb5aa9df6a8360fa8adefd6b174ab215a
enoatu <acro1452@gmail.com>
2024-08-25 15:23:01 +0900

    バッファのエントリ生成ロジックをカスタマイズする関数を追加

--- lua/telescope/_extensions/buffer_scope.lua
+++ lua/telescope/_extensions/buffer_scope.lua
@@ -5,10 +5,13 @@ local sorters = require("telescope.sorters")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local make_entry = require("telescope.make_entry")
+local entry_display = require("telescope.pickers.entry_display")
 local conf = require("telescope.config").values
+local utils = require("telescope.utils")
+local Path = require("plenary.path")
+local strings = require("plenary.strings")
 
 local M = {}
-print("Hello from mytelescope.lua")
 
 function M.buffers(opts)
     opts = opts or {}
@@ -87,7 +90,7 @@ function M.buffers(opts)
             prompt_title = "Buffers",
             finder = finders.new_table({
                 results = buffers,
-                entry_maker = opts.entry_maker or make_entry.gen_from_buffer(opts),
+                entry_maker = opts.entry_maker or M.gen_from_buffer(opts),
             }),
             previewer = conf.grep_previewer(opts),
             sorter = conf.generic_sorter(opts),
@@ -98,6 +101,82 @@ function M.buffers(opts)
             end,
         })
         :find()
+end
+
+function M.gen_from_buffer(opts)
+    opts = opts or {}
+
+    local disable_devicons = opts.disable_devicons
+
+    local icon_width = 0
+    if not disable_devicons then
+        local icon, _ = utils.get_devicons("fname", disable_devicons)
+        icon_width = strings.strdisplaywidth(icon)
+    end
+
+    local displayer = entry_display.create({
+        separator = " ",
+        items = {
+            { width = opts.bufnr_width },
+            { width = 4 },
+            { width = icon_width },
+            { remaining = true },
+        },
+    })
+
+    local cwd = utils.path_expand(opts.cwd or vim.loop.cwd())
+
+    local make_display = function(entry)
+        -- bufnr_width + modes + icon + 3 spaces + : + lnum
+        opts.__prefix = opts.bufnr_width + 4 + icon_width + 3 + 1 + #tostring(entry.lnum)
+        local display_bufname, path_style = utils.transform_path(opts, entry.filename)
+        local icon, hl_group = utils.get_devicons(entry.filename, disable_devicons)
+
+        return displayer({
+            { entry.bufnr, "TelescopeResultsNumber" },
+            { entry.indicator, "TelescopeResultsComment" },
+            { icon, hl_group },
+            {
+                display_bufname .. ":" .. entry.lnum,
+                function()
+                    return path_style
+                end,
+            },
+        })
+    end
+
+    return function(entry)
+        local filename = entry.info.name ~= "" and entry.info.name or nil
+        local bufname = filename and Path:new(filename):normalize(cwd) or "[No Name]"
+
+        local hidden = entry.info.hidden == 1 and "h" or "a"
+        local readonly = vim.api.nvim_buf_get_option(entry.bufnr, "readonly") and "=" or " "
+        local changed = entry.info.changed == 1 and "+" or " "
+        local indicator = entry.flag .. hidden .. readonly .. changed
+        local lnum = 1
+
+        -- account for potentially stale lnum as getbufinfo might not be updated or from resuming buffers picker
+        if entry.info.lnum ~= 0 then
+            -- but make sure the buffer is loaded, otherwise line_count is 0
+            if vim.api.nvim_buf_is_loaded(entry.bufnr) then
+                local line_count = vim.api.nvim_buf_line_count(entry.bufnr)
+                lnum = math.max(math.min(entry.info.lnum, line_count), 1)
+            else
+                lnum = entry.info.lnum
+            end
+        end
+
+        return make_entry.set_default_entry_mt({
+            value = bufname,
+            ordinal = entry.bufnr .. " : " .. bufname,
+            display = make_display,
+            bufnr = entry.bufnr,
+            path = filename,
+            filename = bufname,
+            lnum = lnum,
+            indicator = indicator,
+        }, opts)
+    end
 end
 
 return telescope.register_extension({
